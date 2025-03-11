f4f672aa882684e01db8bb27c260552b2cd7a2cd
Arina <60472565+rinxt@users.noreply.github.com>
2025-03-11 20:25:33 +0300

    Update __init__.py

--- doubly_linked_list/__init__.py
+++ doubly_linked_list/__init__.py
@@ -1,4 +1,5 @@
 from .linked_list import LinkedList
 from .obj_list import ObjList
+from .exceptions import InvalidObjectTypeError
 
-__all__ = ['LinkedList', 'ObjList']
+__all__ = ['LinkedList', 'ObjList', 'InvalidObjectTypeError']
